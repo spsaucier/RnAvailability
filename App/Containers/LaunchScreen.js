3e5dcf1261a7e6f4c168388abf68f9fd4faaf40b
Stephen Saucier <spsaucier@gmail.com>
2018-03-01 11:38:34 -0500

    Availability App all set up & working

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,32 +1,245 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
+import React from 'react'
+import { ScrollView, View, Text, TouchableOpacity, Image, TextInput, Button, DatePickerIOS } from 'react-native'
+import { Metrics, Images } from '../../ignite/DevScreens/DevTheme'
+import FullButton from '../Components/FullButton'
+import moment from "moment-timezone"
 
-import { Images } from '../Themes'
+// For API
+import API from '../Services/GatherApi'
+import FJSON from 'format-json'
 
 // Styles
-import styles from './Styles/LaunchScreenStyles'
+import styles from '../../ignite/DevScreens/Styles/APITestingScreenStyles'
 
-export default class LaunchScreen extends Component {
+export default class APITestingScreen extends React.Component {
+  api = {}
+
+  constructor (props) {
+    super(props)
+    this.initialState = {
+      visibleHeight: Metrics.screenHeight,
+      datetime: new Date(),
+      availableRooms: [],
+      availablePackages: []
+    }
+    this.state = Object.assign({}, this.initialState);
+
+    this.getEndpoints = this.getEndpoints.bind(this);
+    this.setDateTime = this.setDateTime.bind(this);
+    this.api = API.create()
+  }
+
+  applyResult (response, title = 'Response') {
+    this.refs.container.scrollTo({x: 0, y: 0, animated: true})
+    if (response.ok) {
+      if (response.data.data.available) {
+        // Result of Rooms Availability check
+        this.setState({availableRooms: response.data.data.available})
+      }
+      if (response.data.data[0]) {
+        // Result of Packages Availability check
+        this.setState({availablePackages: response.data.data})
+      }
+    } else {
+      this.refs.result.setState({message: `${response.problem} - ${response.status}`, title: title})
+    }
+  }
+
+  // API buttons here:
+  getEndpoints () {
+    return [
+      {
+        label: 'Get Available Rooms',
+        endpoint: 'getAvailableRooms',
+        args: [
+          'axuowl7o',
+          this.state.date || '2018-05-09',
+          this.state.time || '12:00:00',
+          this.state.guests || 30
+        ]
+      },
+      {
+        label: 'Get Available Packages',
+        endpoint: 'getAvailablePackages',
+        args: [
+          'axuowl7o',
+          this.state.room || 'lmef8our',
+          this.state.date || '2018-05-09',
+          this.state.packageTime || '12:00:00',
+          this.state.guests || 30
+        ]
+      }
+    ];
+  }
+
+  tryEndpoint (apiEndpoint) {
+    const { label, endpoint, args = [''] } = apiEndpoint
+    this.api[endpoint].apply(this, args).then((result) => {
+      this.applyResult(result, label || `${endpoint}(${args.join(', ')})`)
+    })
+  }
+
+  renderButton (apiEndpoint) {
+    const { label, endpoint, args = [''] } = apiEndpoint
+    return (
+      <FullButton text={label || `${endpoint}(${args.join(', ')})`} onPress={this.tryEndpoint.bind(this, apiEndpoint)} styles={{marginTop: 10}} key={`${endpoint}-${args.join('-')}`} />
+    )
+  }
+
+  renderRooms (rooms) {
+    return rooms.map((room, index) => {
+      return <View style={styles.section} key={index}>
+        {room.$Photos && room.$Photos[0] && room.$Photos[0].url ? 
+          <Image source={{uri: room.$Photos[0].url}} style={{width: 300, height: 200}} />
+          : null
+        }
+        <Text style={{
+          color: "#000",
+          fontSize: 20,
+          marginTop: 10,
+          marginBottom: 10
+        }}>{room.name}</Text>
+        <Text style={{marginBottom: 5}}>{room.description}</Text>
+        {this.renderStartTimes(room.startTimes, room.id)}
+      </View>
+    })
+  }
+
+  renderStartTimes (times, roomId) {
+    return times.map((time, index) => {
+      return <FullButton key={index} text={time.startTime} onPress={() => {
+        this.setState({packageTime: time.startTime, room: roomId})
+        this.tryEndpoint(this.getEndpoints()[1])
+      }}/>
+    })
+  }
+
+  renderPackages (packages) {
+    return packages.map((myPackage, index) => {
+      return <View style={styles.section} key={index}>
+        {myPackage.photo ? 
+          <Image source={{uri: myPackage.photo}} style={{width: 300, height: 200}} />
+          : null
+        }
+        <Text style={{
+          color: "#000",
+          fontSize: 20,
+          marginTop: 10,
+          marginBottom: 10
+        }}>{myPackage.name}</Text>
+        <Text>{myPackage.description}</Text>
+        <Text style={{
+          color: "#000",
+          fontSize: 18,
+          fontWeight: "bold",
+          marginTop: 5,
+        }}>${myPackage.price}</Text>
+      </View>
+    })
+  }
+
+  setDateTime(newDateTime) {
+    let state = {
+      datetime: newDateTime,
+      date: moment(newDateTime).format("YYYY-MM-DD"),
+      time: moment(newDateTime).format("HH:mm:ss"),
+    }
+    this.setState(state)
+  }
+
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-          <DevscreensButton />
-        </ScrollView>
+        <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{
+          position: 'absolute',
+          paddingTop: 30,
+          paddingHorizontal: 5,
+          zIndex: 10
+        }}>
+          <Image source={Images.backButton} />
+        </TouchableOpacity>
+        { !this.state.availableRooms.length ? 
+          <ScrollView style={styles.container} ref='container'>
+            <View style={{alignItems: 'center', paddingTop: 60}}>
+              <Text style={{fontSize: 28}}>Check Availability</Text>
+              <Text>Fleming's Steakhouse Atlanta</Text>
+            </View>
+            <View style={styles.section}>
+              <Text>When would you like to book?</Text>
+              <DatePickerIOS
+                date={this.state.datetime}
+                onDateChange={this.setDateTime}
+                minimumDate={new Date()}
+                minuteInterval={30}
+              />
+              <Text>Guest Count</Text>
+              <TextInput
+                style={{height: 40, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.guests}
+                keyboardType="numeric"
+              />
+            </View>
+            {this.renderButton(this.getEndpoints()[0])}
+            <APIResult ref='result' />
+          </ScrollView>
+        :
+          !this.state.availablePackages.length ?
+            <ScrollView style={styles.container} ref='container'>
+              <View style={{alignItems: 'center', paddingTop: 60}}>
+                {this.renderRooms(this.state.availableRooms)}
+              </View>
+              <FullButton onPress={() => this.setState({availableRooms: []})} text="Back to Search"/>
+            </ScrollView>
+          :
+            <ScrollView style={styles.container} ref='container'>
+              <View style={{alignItems: 'center', paddingTop: 60}}>
+                {this.renderPackages(this.state.availablePackages)}
+              </View>
+              <FullButton onPress={() => this.setState({availablePackages: []})} text="Back to Rooms"/>
+            </ScrollView>
+        }
+          
       </View>
     )
+  }
+}
+
+class APIResult extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      message: false,
+      title: false
+    }
+  }
+
+  onApiPress = () => {
+    this.setState({message: false})
+  }
+
+  renderView () {
+    return (
+      <ScrollView style={{ top: 0, bottom: 0, left: 0, right: 0, position: 'absolute' }} overflow='hidden'>
+        <TouchableOpacity
+          style={{backgroundColor: 'white', padding: 20}}
+          onPress={this.onApiPress}
+        >
+          <Text>{this.state.title} Response:</Text>
+          <Text allowFontScaling={false} style={{fontFamily: 'CourierNewPS-BoldMT', fontSize: 10}}>
+            {this.state.message}
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    )
+  }
+
+  render () {
+    let messageView = null
+    if (this.state.message) {
+      return this.renderView()
+    }
+
+    return messageView
   }
 }
--- ignite/DevScreens/APITestingScreen.js
+++ ignite/DevScreens/APITestingScreen.js
@@ -1,42 +1,69 @@
 import React from 'react'
-import { ScrollView, View, Text, TouchableOpacity, Image } from 'react-native'
+import { ScrollView, View, Text, TouchableOpacity, Image, TextInput, Button, DatePickerIOS } from 'react-native'
 import { Metrics, Images } from './DevTheme'
 import FullButton from '../../App/Components/FullButton'
+import moment from "moment-timezone"
 
 // For API
-import API from '../../App/Services/Api'
+import API from '../../App/Services/GatherApi'
 import FJSON from 'format-json'
 
 // Styles
 import styles from './Styles/APITestingScreenStyles'
-
-// API buttons here:
-const endpoints = [
-  { label: 'Github Root', endpoint: 'getRoot' },
-  { label: 'Github Rate Limit', endpoint: 'getRate' },
-  { label: 'Search User (gantman)', endpoint: 'getUser', args: ['gantman'] },
-  { label: 'Search User (skellock)', endpoint: 'getUser', args: ['skellock'] }
-]
 
 export default class APITestingScreen extends React.Component {
   api = {}
 
   constructor (props) {
     super(props)
-    this.state = {
-      visibleHeight: Metrics.screenHeight
+    this.initialState = {
+      visibleHeight: Metrics.screenHeight,
+      datetime: new Date(),
+      availableRooms: [],
+      availablePackages: []
     }
+    this.state = Object.assign({}, this.initialState);
 
+    this.getEndpoints = this.getEndpoints.bind(this);
+    this.setDateTime = this.setDateTime.bind(this);
     this.api = API.create()
   }
 
   showResult (response, title = 'Response') {
     this.refs.container.scrollTo({x: 0, y: 0, animated: true})
     if (response.ok) {
-      this.refs.result.setState({message: FJSON.plain(response.data), title: title})
+      if (response.data.data.available) this.setState({availableRooms: response.data.data.available})
+      if (response.data.data[0]) this.setState({availablePackages: response.data.data})
     } else {
       this.refs.result.setState({message: `${response.problem} - ${response.status}`, title: title})
     }
+  }
+
+  // API buttons here:
+  getEndpoints () {
+    return [
+      {
+        label: 'Get Available Rooms',
+        endpoint: 'getAvailableRooms',
+        args: [
+          'axuowl7o',
+          this.state.date || '2018-05-09',
+          this.state.time || '12:00:00',
+          this.state.guests || 30
+        ]
+      },
+      {
+        label: 'Get Available Packages',
+        endpoint: 'getAvailablePackages',
+        args: [
+          'axuowl7o',
+          this.state.room || 'lmef8our',
+          this.state.date || '2018-05-09',
+          this.state.packageTime || '12:00:00',
+          this.state.guests || 30
+        ]
+      }
+    ];
   }
 
   tryEndpoint (apiEndpoint) {
@@ -53,14 +80,60 @@ export default class APITestingScreen extends React.Component {
     )
   }
 
-  renderButtons () {
-    return endpoints.map((endpoint) => this.renderButton(endpoint))
+  renderRooms (rooms) {
+    return rooms.map((room, index) => {
+      return <View style={styles.section} key={index}>
+        {room.$Photos && room.$Photos[0] && room.$Photos[0].url ? 
+          <Image source={{uri: room.$Photos[0].url}} style={{width: 300, height: 200}} />
+          : null
+        }
+        <Text style={{
+          color: "#fff",
+          fontSize: 20,
+          marginTop: 10,
+          marginBottom: 10
+        }}>{room.name}</Text>
+        <Text style={styles.titleText}>{room.description}</Text>
+        {this.renderStartTimes(room.startTimes, room.id)}
+      </View>
+    })
+  }
+
+  renderStartTimes (times, roomId) {
+    return times.map((time, index) => {
+      return <FullButton key={index} text={time.startTime} onPress={() => {
+        this.setState({packageTime: time.startTime, room: roomId})
+        this.tryEndpoint(this.getEndpoints()[1])
+      }}/>
+    })
+  }
+
+  renderPackages (packages) {
+    return packages.map((myPackage, index) => {
+      return <View style={styles.section} key={index}>
+        {myPackage.photo ? 
+          <Image source={{uri: myPackage.photo}} style={{width: 300, height: 200}} />
+          : null
+        }
+        <Text style={styles.titleText}>{myPackage.name}</Text>
+        <Text style={styles.titleText}>{myPackage.description}</Text>
+        <Text style={styles.titleText}>${myPackage.price}</Text>
+      </View>
+    })
+  }
+
+  setDateTime(newDateTime) {
+    let state = {
+      datetime: newDateTime,
+      date: moment(newDateTime).format("YYYY-MM-DD"),
+      time: moment(newDateTime).format("HH:mm:ss"),
+    }
+    this.setState(state)
   }
 
   render () {
     return (
       <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
         <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={{
           position: 'absolute',
           paddingTop: 30,
@@ -69,23 +142,48 @@ export default class APITestingScreen extends React.Component {
         }}>
           <Image source={Images.backButton} />
         </TouchableOpacity>
-        <ScrollView style={styles.container} ref='container'>
-          <View style={{alignItems: 'center', paddingTop: 60}}>
-            <Image source={Images.api} style={styles.logo} />
-            <Text style={styles.titleText}>API</Text>
-          </View>
-          <View style={styles.section}>
-            <Text style={styles.sectionText}>
-              Testing API with Postman or APIary.io verifies the server works.
-              The API Test screen is the next step; a simple in-app way to verify and debug your in-app API functions.
-            </Text>
-            <Text style={styles.sectionText}>
-              Create new endpoints in Services/Api.js then add example uses to endpoints array in Containers/APITestingScreen.js
-            </Text>
-          </View>
-          {this.renderButtons()}
-          <APIResult ref='result' />
-        </ScrollView>
+        { !this.state.availableRooms.length ? 
+          <ScrollView style={styles.container} ref='container'>
+            <View style={{alignItems: 'center', paddingTop: 60}}>
+              <Image source={Images.api} style={styles.logo} />
+              <Text style={styles.titleText}>API</Text>
+            </View>
+            <View style={styles.section}>
+              <Text style={styles.sectionText}>When would you like to book?</Text>
+              <DatePickerIOS
+                date={this.state.datetime}
+                onDateChange={this.setDateTime}
+                minimumDate={new Date()}
+                minuteInterval={30}
+              />
+              <Text style={styles.sectionText}>Guest Count</Text>
+              <TextInput
+                style={{height: 40, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.guests}
+                keyboardType="numeric"
+              />
+            </View>
+            {this.renderButton(this.getEndpoints()[0])}
+            <APIResult ref='result' />
+          </ScrollView>
+        :
+          !this.state.availablePackages.length ?
+            <ScrollView style={styles.container} ref='container'>
+              <View style={{alignItems: 'center', paddingTop: 60}}>
+                {this.renderRooms(this.state.availableRooms)}
+              </View>
+              <FullButton onPress={() => this.setState({availableRooms: []})} text="Back to Search"/>
+            </ScrollView>
+          :
+            <ScrollView style={styles.container} ref='container'>
+              <View style={{alignItems: 'center', paddingTop: 60}}>
+                {this.renderPackages(this.state.availablePackages)}
+              </View>
+              <FullButton onPress={() => this.setState({availablePackages: []})} text="Back to Rooms"/>
+            </ScrollView>
+        }
+          
       </View>
     )
   }
